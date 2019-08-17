61673e577fe6fb5fa9fcb70ff6eabb7c8bbfa6da
Tamas Szamel <Tamas_Szamel@epam.com>
2019-08-17 15:42:24 +0200

    hello world routes

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  # 127.0.0.1:5000
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/country')  # 127.0.0.1:5000/country
+def my_country():
+    return '<h1>Hello Hungary!</h1>'
+
 
 if __name__ == '__main__':
     app.run()
