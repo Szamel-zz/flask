cf090418263db7a1ffaac8c2d8bb555b95aa71cd
Tamas Szamel <Tamas_Szamel@epam.com>
2019-08-18 21:49:02 +0200

    template variables jinja

--- flask_examples/basic.py
+++ flask_examples/basic.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+'''
+Folder name are mandatory:
+static
+templates
+'''
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    greetings = "Lok'tar Ogar!"
+    my_list = [1, 2, 3, 4]
+    sum_of_my_list = sum(my_list)
+    my_dict = {'race': 'orc'}
+    return render_template('basic.html',
+                           my_variable=greetings,
+                           my_list=my_list,
+                           my_sum=sum_of_my_list,
+                           my_dict=my_dict)
 
 
 if __name__ == '__main__':
